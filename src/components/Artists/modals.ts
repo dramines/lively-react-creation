465ec7fb3b64cb5ad5fad478ada3067692452651
dramines <dramines@draminesaid.com>
2025-03-20 13:16:39 +0100

    oki

--- src/components/Artists/modals.ts
+++
@@ -1,4 +0,0 @@
-
-export { default as ProjectModal } from './Modals/ProjectModal';
-export { default as TaskModal } from './Modals/TaskModal';
-export { default as FileUploadModal } from './Modals/FileUploadModal';
